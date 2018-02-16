34abbe184b0dff2fff801453430f187da2a338eb
eal6gf <elum1234@gmail.com>
2018-02-16 12:01:26 -0500

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+
+  handleDelete = () => {
+    this.props.store.dispatch({
+      type: "DELETE_BAND",
+      id: this.props.band.id
+    })
+  }
+
   render() {
     return(
       <div>
-        band component
+        <ul>
+          <li>
+              {this.props.band.text}
+              <button onClick={this.handleDelete}>Delete</button>
+          </li>
+        </ul>
       </div>
     );
   }
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-
+import Band from './Band'
 class Bands extends Component {
   render() {
+    let renderBands = this.props.store.getState().bands.map((band,index)=> <Band key={index} store={this.props.store} band={band}/>)
+
+    console.log(renderBands)
     return(
       <ul>
-        Bands Component
+        {renderBands}
       </ul>
     );
   }
