3a4c9e3b98beeab6c4570374391cc5d02b48a8cf
ddbrennan <ddbren@gmail.com>
2018-02-16 11:49:44 -0500

    add and delete bands

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Band from './Band'
 
 class Bands extends Component {
   render() {
     return(
       <ul>
-        Bands Component
+        {this.props.store.getState().bands.map(b => <Band key={b.id} band={b} store={this.props.store}/>)}
       </ul>
     );
   }
